2e38dbe212dd702765f4b06afdc4f26398f2a2f3
Auberon López <auberon@adadev.org>
2022-04-23 23:07:53 -0700

    Deleted dead code

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,10 +3,6 @@ import { Reorder } from "framer-motion";
 import Item from "./Item";
 
 function ItemList({ data, setData }) {
-  //const items = data.map((e) => <Item key={e} title={e}></Item>);
-
-  //return <div>{items}</div>;
-
   return (
     <div>
       <Reorder.Group values={data} onReorder={setData}>
@@ -21,32 +17,3 @@ function ItemList({ data, setData }) {
 }
 
 export default ItemList;
-/*
-
-import { useState } from "react";
-import { Reorder } from "framer-motion";
-
-export default function App() {
-  const [items, setItems] = useState([0, 1, 2, 3]);
-
-  const swap = () => {
-    const newItems = [...items];
-    [newItems[0], newItems[1]] = [newItems[1], newItems[0]];
-    setItems(newItems);
-  };
-
-  return (
-    <div>
-      <Reorder.Group values={items} onReorder={setItems}>
-        {items.map((item) => (
-          <Reorder.Item key={item} value={item} dragListener={false}>
-            {item}
-          </Reorder.Item>
-        ))}
-      </Reorder.Group>
-      <button onClick={swap}>swap</button>
-    </div>
-  );
-}
-
-*/
