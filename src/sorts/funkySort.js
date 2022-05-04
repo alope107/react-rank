e979272f08d85676d1c63e80cd32f35e2121c34e
Auberon López <auberon@adadev.org>
2022-05-03 21:05:39 -0700

    Add rough draft of funkysort.
    - Created a new sorting algo, funkysort. It tries to have a similar number of comparisons to insertion sort, but to have those comparisons appear in a much more random order.
    
    - Many improvements still needed. Currently there is poor duplicate checking, unseeded random calls in tests, general ugliness, etc.
    
    - UI still uses insertion sort.

---
+++ src/sorts/funkySort.js
@@ -0,0 +1,74 @@
+// TODO(auberon): Move this to better location and get seedable random
+function randInt(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+// TODO(auberon): Move this to better location and get seedable random
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+/**
+ * Does a insertion sort bit by bit. It non-blockingly delegates
+ * comparisons to some outside comparator. It uses updatePair
+ * to denote the two items it wants compared. The outside
+ * comparator should then call next on the iterator and pass
+ * true if the first item in the pair should be before the second,
+ * false otherwise.
+ *
+ * @param {Array} arr The initial data array
+ * @param {function(Array)} updateArr The state updater for arr
+ * @param {function(Array)} updatePair The state updater for the pair to check
+ */
+function* funkySort(arr, updateArr, updatePair) {
+  console.log("FUNKY TIME");
+  function swap(i, j) {
+    arr = [...arr];
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    updateArr(arr);
+  }
+
+  if (arr.length === 0) {
+    return;
+  }
+
+  const unchecked_positions = [...Array(arr.length - 1).keys()];
+  shuffleArray(unchecked_positions);
+  const checked_pairs = new Set();
+
+  while (unchecked_positions.length > 0) {
+    console.log(unchecked_positions);
+    const position_idx = randInt(0, unchecked_positions.length - 1);
+    const position = unchecked_positions[position_idx];
+    unchecked_positions.splice(position_idx, 1);
+
+    const pair = [arr[position], arr[position + 1]];
+    pair.sort();
+    // TODO(auberon): Fix this!!! Does not properly account for items with commas
+    const pairStr = pair.toString();
+    if (!checked_pairs.has(pairStr)) {
+      updatePair([arr[position + 1], arr[position]]);
+      const shouldSwap = yield;
+      if (shouldSwap) {
+        swap(position, position + 1);
+        //if pos > 0 and (pos-1) not in unchecked_positions:
+        if (position > 0 && !unchecked_positions.includes(position - 1)) {
+          unchecked_positions.push(position - 1);
+        }
+        //if pos < len(data)-2 and (pos+1) not in unchecked_positions:
+        if (
+          position < arr.length - 2 &&
+          !unchecked_positions.includes(position + 1)
+        ) {
+          unchecked_positions.push(position + 1);
+        }
+      }
+      checked_pairs.add(pairStr);
+    }
+  }
+}
+
+export default funkySort;
