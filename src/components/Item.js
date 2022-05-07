dd53684776ebceb2951af9f89965381b46bdb878
Auberon López <auberon@adadev.org>
2022-05-06 22:20:21 -0700

    Switch to useAnimation for Item.
    - Switched to explict animation controls for Item component.
    
    - Added ability to trigger animations imperatively for Item.
    Currently just added a flash of red when clicked.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,31 +1,50 @@
 import PropTypes from "prop-types";
 
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 
 function Item({ content }) {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start(() => ({
+      // animation on entry
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 700,
+        damping: 15,
+      },
+    }));
+  }, []);
+
+  // Flash red background and then go back to transparent
+  const flash = () => {
+    controls.start({
+      backgroundColor: [
+        "rgba(255, 0, 0, 0)",
+        "rgba(255, 0, 0, 1)",
+        "rgba(255, 0, 0, 0)",
+      ],
+      transition: { duration: 0.5, ease: "easeInOut" },
+    });
+  };
+
   return (
     <motion.div
       layout
       key={content}
       value={content}
-      dragListener={false}
       initial={{ opacity: 0, y: 30 }}
-      animate={{
-        // animation on entry
-        opacity: 1,
-        y: 0,
-        transition: {
-          type: "spring",
-          stiffness: 700,
-          damping: 15,
-        },
-      }}
+      animate={controls}
       transition={{
         //animation on reorder
         type: "spring",
         stiffness: 500,
         damping: 20,
       }}
+      onClick={flash}
     >
       <h1>{content}</h1>
     </motion.div>
